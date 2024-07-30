410a897b32c9834eed16cb59e5fa23121cfb43d2
mohyerolo <garam2101@naver.com>
2024-07-30 19:19:30 +0900

    gitignore  적용:

---
+++ src/main/java/com/pesonal/FindDogPlz/FindDogPlzApplication.java
@@ -0,0 +1,13 @@
+package com.pesonal.FindDogPlz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindDogPlzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindDogPlzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pesonal/FindDogPlz/FindDogPlzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesonal.FindDogPlz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindDogPlzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
