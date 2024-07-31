ea1a9ae35690fec2f4345a0986ff9831b9853d3f
mohyerolo <garam2101@naver.com>
2024-07-31 11:25:04 +0900

    feat: Member, MemberAdapter 추가

---
+++ src/main/java/com/pesonal/FindDogPlz/domain/Member.java
@@ -0,0 +1,38 @@
+package com.pesonal.FindDogPlz.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member extends BaseDateEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String loginId;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private Point point;
+
+    @ElementCollection
+    private List<String> roles = new ArrayList<>();
+}
---
+++ src/main/java/com/pesonal/FindDogPlz/domain/MemberAdapter.java
@@ -0,0 +1,27 @@
+package com.pesonal.FindDogPlz.domain;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class MemberAdapter extends User {
+
+    private final Member member;
+
+    public MemberAdapter(Member member){
+        super(member.getLoginId(),member.getPassword(), authorities(member.getRoles()));
+        this.member = member;
+    }
+
+    private static Collection<? extends GrantedAuthority> authorities(List<String> roles) {
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
