1f466f36d3cb5bc7c69f87914789b139ecec1814
mohyerolo <garam2101@naver.com>
2024-08-04 17:38:03 +0900

    feat: MethodArgumentNotValidException 핸들러 추가

--- src/main/java/com/pesonal/FindDogPlz/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/pesonal/FindDogPlz/global/exception/CustomExceptionHandler.java
@@ -2,6 +2,8 @@ package com.pesonal.FindDogPlz.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +15,13 @@ public class CustomExceptionHandler {
     public ResponseEntity<ErrorResponseDto> handleCustomException(CustomException exception) {
         log.error(exception.toString());
         return ErrorResponseDto.toResponseEntity(exception.getErrorCode(), exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        FieldError fieldError = exception.getBindingResult().getFieldError();
+        String message = fieldError.getField() + ": " + fieldError.getDefaultMessage();
+        log.info("[CustomExceptionHandler] - @Valid 에러 {}", message);
+        return ErrorResponseDto.toResponseEntity(ErrorCode.BAD_REQUEST, message);
     }
 }
