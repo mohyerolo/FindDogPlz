4f09f4f7bff032b05e6b668070e14e3f6c7e3a65
mohyerolo <garam2101@naver.com>
2024-10-10 12:12:41 +0900

    update: 웹소켓 접속 시 불필요한 중복 jwt 검사 수정

--- src/main/java/com/pesonal/FindDogPlz/global/config/WebSocketAuthInterceptor.java
+++ src/main/java/com/pesonal/FindDogPlz/global/config/WebSocketAuthInterceptor.java
@@ -1,16 +1,12 @@
 package com.pesonal.FindDogPlz.global.config;
 
-import com.pesonal.FindDogPlz.global.auth.JwtTokenProvider;
 import com.pesonal.FindDogPlz.global.exception.CustomException;
-import com.pesonal.FindDogPlz.global.exception.ErrorCode;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.http.server.ServletServerHttpResponse;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
@@ -21,34 +17,19 @@ import java.util.Map;
 @Component
 @RequiredArgsConstructor
 public class WebSocketAuthInterceptor implements HandshakeInterceptor {
-
-    private final JwtTokenProvider jwtTokenProvider;
-
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-        if (request instanceof ServletServerHttpRequest) {
-            HttpServletRequest servletRequest = ((ServletServerHttpRequest) request).getServletRequest();
-            HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        if (!(request instanceof ServletServerHttpRequest)) return false;
 
-            /*
-              나중에 jwt 토큰값으로 인증하는 걸로 바꾸기? -> 지금 챗 연결은 JwtAuthenticationFilter를 거치고 있음.
-              나중에 챗 주고 받을 때도 그러면 그냥 이걸로 해도 상관없을듯
-             */
-            String token = servletRequest.getHeader("Authorization");
-            if (token == null || !jwtTokenProvider.validateToken(token)) {
-                throw new CustomException(ErrorCode.AUTHENTICATION_ERROR, "채팅은 인증된 사용자만 가능합니다.");
-            }
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            if (authentication!= null) {
-                attributes.put("sender", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-                attributes.put("chatRoomId", servletRequest.getHeader("chatRoomId"));
-                return true;
-            } else {
-                servletResponse.setStatus(401);
-                return false;
-            }
+        HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        try {
+            attributes.put("sender", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+            return true;
+        } catch (CustomException e) {
+            servletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            servletResponse.getWriter().write(e.getMessage());
+            return false;
         }
-        return false;
     }
 
     @Override
