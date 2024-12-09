68be92ce819a0fd9fafbb6c4da9b031d77d3fa5e
mohyerolo <garam2101@naver.com>
2024-12-09 14:36:01 +0900

    fix: point null일 경우 에러 발생, 미리 에러 발생하도록 수정

--- src/main/java/com/pesonal/FindDogPlz/global/util/PointParser.java
+++ src/main/java/com/pesonal/FindDogPlz/global/util/PointParser.java
@@ -7,13 +7,18 @@ import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKTReader;
 
 public class PointParser {
-    public static Point parsePoint(Double latitude, Double longitude) {
+    public static Point parsePoint(final Double latitude, final Double longitude) {
         try {
-            return latitude != null && longitude != null ?
-                    (Point) new WKTReader().read(String.format("POINT(%s %s)", longitude, latitude))
-                    : null;
+            return parseUsingReader(latitude, longitude);
         } catch (ParseException e) {
             throw new CustomException(ErrorCode.POINT_PARSING_ERROR);
         }
+    }
+
+    private static Point parseUsingReader(final Double latitude, final Double longitude) throws ParseException {
+        if (latitude == null && longitude == null) {
+            throw new CustomException(ErrorCode.POINT_PARSING_ERROR);
+        }
+        return (Point) new WKTReader().read(String.format("POINT(%s %s)", longitude, latitude));
     }
 }
